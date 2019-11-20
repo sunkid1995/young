d79166db668e36d3339fb5ffc17843eb6ccc9982
binhnn <binhnn@onaclover.com>
2019-11-20 11:57:17 +0700

    Eslint: setup eslint and commons file editor config

--- src/Views/App/index.js
+++ src/Views/App/index.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
+const INIT_STATE = {
+  name: 'Hello world',
+  age: 25,
+  city: 'Ha Noi',
+};
+
 function App() {
+  const [ state, setState ] = useState(INIT_STATE);
+
+  // function inputChange(key, e) {
+  //   return setState({ [key]: e.target.value });
+  // }
+
   return (
     <div className="App">
-        {'Hello'}
+      <p><span>{'Name:'}</span>{state.name}</p>
     </div>
   );
 }
