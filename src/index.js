81310373a49a560f15a696cf74cf0065c9e7c562
binhnn <binhnn@onaclover.com>
2019-11-20 15:12:23 +0700

    Router: init router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 // Views
 import App from './views/App';
@@ -10,7 +11,13 @@ import * as serviceWorker from './serviceWorker';
 // Css
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const RenderApp = () => (
+  <Router>
+    <App />
+  </Router>
+);
+
+ReactDOM.render(RenderApp(), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
